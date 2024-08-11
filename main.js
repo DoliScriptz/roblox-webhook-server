3faa0150cf608e61440cef603ac7b83d2a3caa8b
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2024-08-11 18:13:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,9 @@ app.use(express.json());
 
 const webhookUrl = "https://discord.com/api/webhooks/1262390634862346250/mofvVvsxnwggu0Q9qqF90YrmlFn5kLGxRveT9xfivDQhucp_VYON-nHgCZaL-bOvcYP3";
 
-// Log server start
-console.log("Starting server...");
-
-// Define webhook endpoint
 app.post("/webhook", async (req, res) => {
   const { executorName } = req.body;
-
-  // Log incoming request
-  console.log("Received request for executor:", executorName);
+  console.log("Received request:", req.body);
 
   const joinScript = `local teleportService = game:GetService('TeleportService')
 local players = game:GetService('Players')
@@ -35,7 +29,6 @@ teleportService:TeleportToPlaceInstance(game.PlaceId, targetPlayer)
 print('Player not found.')
 }`;
 
-  // Construct webhook message
   const data = {
     content: null,
     embeds: [{
@@ -52,27 +45,10 @@ ${joinScript}
   };
 
   try {
-    // Send webhook message
-    await fetch(webhookUrl, {
+    const response = await fetch(webhookUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
     });
-
-    // Log webhook sent
-    console.log(`Webhook sent for executor: ${executorName}`);
-    res.status(200).send('Webhook sent');
-  } catch (error) {
-    // Log error if failed
-    console.error("Error sending webhook:", error);
-    res.status(500).send('Error sending webhook');
-  }
-});
-
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
